8b93cd0700a09499fe930460862d6b7715a7b5d5
thefr33radical <imperial.gauntlet@gmail.com>
2018-06-21 16:34:46 +0530

    21jun18

---
+++ hyperparameter_tuning/grid_search.py
@@ -0,0 +1,12 @@
+
+
+class Grid(object):
+
+    class Regression(object):
+
+
+    class Classification(object):
+        def svm(self,train_input,train_output,test_input,test_output):
+            pass
+        def logistic_regression(self,train_input,train_output,test_input,test_output):
+            pass
