6803ae5d22e046f21b4d6311418c5831ac89f16f
thefr33radical <imperial.gauntlet@gmail.com>
2018-06-25 15:36:15 +0530

    25jun18

--- hyperparameter_tuning/grid_search.py
+++ hyperparameter_tuning/grid_search.py
@@ -1,17 +1,55 @@
+"""
+
+GridSearch class implements brute force Hypertuning method selection of
+
+
+
+
+"""
+
 from sklearn import linear_model
 from sklearn import svm
-from sklearn import li
+from sklearn import datasets
 
 class GridSearch(object):
 
     class Regression(object):
+        """
+
+        """
         def linear_regression(self,train_input,train_output,test_input,test_output):
+            """
+            :param train_input:
+            :param train_output:
+            :param test_input:
+            :param test_output:
+            :return:
+            """
             pass
         def svr(self,train_input,train_output,test_input,test_output):
-            svm_paramters={{'kernel'="rbf",'c'=[]},{'kernel'="rbf",'c'=[]}}
+            svm_paramters= {{'kernel':"rbf",'c':[]},{'kernel':"rbf",'c':[]}}
             model=svm.SVR()
             pass
         def lasso(self,train_input,train_output,test_input,test_output):
+            pass
+
+        def ridge(self, train_input, train_output, test_input, test_output):
+            """
+            :param train_input: training features
+            :param train_output: training target
+            :param test_input: test features
+            :param test_output: test target
+            :return:
+            """
+
+            dataset = datasets.load_diabetes()
+            model = linear_model.Ridge()
+            ridge_grid_param = {"alpha":[ 0, 0.1,0.001,0.0001,1]}
+            grid_search = GridSearch(model, param_grid = ridge_grid_param)
+            grid_search.fit(dataset.data , dataset.target)
+
+            best_alpha = grid_search.best_estimator_.alpha
+            best_score = grid_search.best_score_
             pass
 
 
--- hyperparameter_tuning/random_search.py
+++ hyperparameter_tuning/random_search.py
@@ -1,3 +1,6 @@
+from sklearn import linear_model
+from sklearn import svm
+from sklearn.model_selection import RandomizedSearchCV
 
 
 class RandomSearch(object):
@@ -5,9 +8,34 @@ class RandomSearch(object):
     class Regression(object):
         def linear_regression(self,train_input,train_output,test_input,test_output):
             pass
+
         def svr(self,train_input,train_output,test_input,test_output):
+            """
+            :param train_input:
+            :param train_output:
+            :param test_input:
+            :param test_output:
+            :return:
+            """
+            model=svm.SVR()
+
             pass
         def lasso(self,train_input,train_output,test_input,test_output):
+            pass
+
+        def ridge(self, train_input, train_output, test_input, test_output):
+            """
+            :param train_input:
+            :param train_output:
+            :param test_input:
+            :param test_output:
+            :return:
+            """
+            ridge_random_param = {"alpha" : []}
+            model = linear_model.Ridge()
+            random_search =  RandomizedSearchCV(model, param_distributions = ridge_random_param)
+
+
             pass
 
 
