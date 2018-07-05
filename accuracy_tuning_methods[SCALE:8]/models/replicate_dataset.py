9873e82dc692ded5c63eeade88a19522e8dc6c6f
thefr33radical <imperial.gauntlet@gmail.com>
2018-07-05 14:23:38 +0530

    5jul18

--- accuracy_tuning_methods[SCALE:8]/models/replicate_dataset.py
+++ accuracy_tuning_methods[SCALE:8]/models/replicate_dataset.py
@@ -17,5 +17,14 @@ def replicator(NO_OF_ROWS,PATH1,PATH2):
     print(len(dataset))
 
 
+def csv_reader(PATH):
+    data = pd.read_csv("/home/kuliza227/Downloads/YearPredictionMSD.txt",sep=",")
+    new_data=data.iloc[:500000,:11]
+    new_data.columns = ["AGE",	"SEX","CREDIT_SCORE",	"CIBIIL_SCORE",	"LOAN_AMT",	"ANNUAL_INCOME", "DURATION","RATING","INTEREST_RATE"," BALANCE", "RESULT"]
+    new_data.to_csv("new_data.csv")
+    print(new_data)
+
+
 if __name__=="__main__":
-    replicator(int(input()),input(),input())
+    #replicator(int(input()),input(),input())
+    csv_reader("")
