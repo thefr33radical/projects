d2cb3cd173cabab9843412bd3338f845d0fbb82b
Gowtham Nayak <imperial.gauntlet@gmail.com>
2018-09-14 14:24:14 +0530

    14sep18

--- feature_selection/pca.py
+++ feature_selection/pca.py
@@ -1,12 +1,10 @@
-
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
-from ..models_classifciation import MODELS_CLASSIFICATION
-from ..models_regression import MODELS_REGRESSION
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from  sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
 import pandas as pd
 import numpy as np
@@ -14,23 +12,29 @@ import numpy as np
 
 class FeatureSelection(object):
 
-    def read_data(self,path):
+    def read_data(self, path):
         """
         Read data from CSV file
         :param path: string
         :return: pandas dataframe
         """
-        dataset = pd.read_csv(path,low_memory=False)
+        dataset = pd.read_csv(path, low_memory=False)
         return dataset
 
-    def split_dataset(self,dataset):
+    def split_dataset(self, dataset):
         """
         Fucnction to split datset into train & test sets
         :param dataset: pandas dataframe
         :return: train/test split data
         """
-        data=dataset[:,:-2]
-        y=dataset[:,-1]
+        data = dataset.iloc[:, :-2]
+        y = dataset.iloc[:, -1]
+
+
+        data = data.replace([np.inf, -np.inf], np.nan).dropna(axis=1)
+        data = data.replace({np.nan: 0})
+
+        data = data.fillna(0)
         train_input, test_input, train_output, test_output = train_test_split(data, y, test_size=0.3,
                                                                               shuffle=True)
         return train_input, test_input, train_output, test_output
@@ -78,75 +82,71 @@ class FeatureSelection(object):
         # All the models score with model name is stored in a csv file
         output.to_csv("output_score.csv", sep=",")
 
-    def classifiers(self, train_input, test_input, train_output, test_output):
-	mnb_classifier = MultinomialNB()
-	rf_classifier = RandomForestClassifier(n_estimators=100, oob_score=True, criterion="entropy", n_jobs=-1,
-                                                    random_state=50,
-                                                    max_depth=200)
-	knn_classifier = KNeighborsClassifier()
-	svc_classifier = SVC(probability=True, kernel="linear")
-	lr_classifier = LogisticRegression(n_jobs=-1)
-	
-	mnb_classifier.fit(train_input,train_output)
-	rf_classifier.fit(train_input,train_output)
-	knn_classifier.fit(train_input,train_output)
-	svc_classifier.fit(train_input,train_output)
-	lr_classifier.fit(train_input,train_output)
+    def train_classifiers(self, train_input, test_input, train_output, test_output, step_size):
+        """
 
-	return accuracy_score(test_output,mnb_classifier.predict(test_input))* 100,
-	accuracy_score(test_output,rf_classifier.predict(test_input))* 100,
-	accuracy_score(test_output,knn_classifier.predict(test_input))* 100,
-	accuracy_score(test_output,svc_classifier.predict(test_input))* 100,
-	accuracy_score(test_output,lr_classifier.predict(test_input))* 100
-	
+        :param train_input:
+        :param test_input:
+        :param train_output:
+        :param test_output:
+        :param step_size:
+        :return: dictionary of results
+        """
+        mnb_classifier = MultinomialNB()
+        rf_classifier = RandomForestClassifier(n_estimators=100, oob_score=True, criterion="entropy", n_jobs=-1,
+                                               random_state=50,
+                                               max_depth=200)
+        knn_classifier = KNeighborsClassifier()
+        svc_classifier = SVC(probability=True, kernel="linear")
+        lr_classifier = LogisticRegression(n_jobs=-1)
 
-	
+       # mnb_classifier.fit(train_input, train_output)
+        rf_classifier.fit(train_input, train_output)
+        knn_classifier.fit(train_input, train_output)
+        svc_classifier.fit(train_input, train_output)
+        lr_classifier.fit(train_input, train_output)
 
-        
+        return { "feature_size" : str(step_size),
+                                 "RF": accuracy_score(test_output, rf_classifier.predict(test_input)) * 100,
+                                 "KNN": accuracy_score(test_output, knn_classifier.predict(test_input)) * 100,
+                                 "SVC": accuracy_score(test_output, svc_classifier.predict(test_input)) * 100,
+                                 "LR":accuracy_score(test_output, lr_classifier.predict(test_input)) * 100
+            }
 
-    def ensemble_regression(self, train_input, test_input, train_output, test_output):
-        pass
 
-    def pca_model_classification(self, train_input, test_input, train_output, test_output):
-            """
-            PCA model to generate and select the best features
+    def pca_model_classification(self, train_input, test_input, train_output, test_output, step_size):
+        """
+        PCA model to generate and select the best features
+        :param train_input:
+        :param test_input:
+        :param train_output:
+        :param test_output:
+        :return: None
+        """
+        count_of_features = len(train_input.columns)
+        global_r2_score = 0
+        global_mean_score = 999999
 
-            :param train_input:
-            :param test_input:
-            :param train_output:
-            :param test_output:
-            :return:
-            """
-            count_of_features = len(train_input.columns)
-            global_r2_score = 0
-            global_mean_score = 999999
+        l = []
+        # MODELS_GENERAL comprises of models that are used to train newly constructed features.
+        count = 100
+        while count < count_of_features:
+            selector = PCA(n_components=count)
+            selector = selector.fit(train_input, train_output)
+            temp_train_input = selector.transform(train_input)
+            temp_test_input = selector.transform(test_input)
 
-            l = []
-            # MODELS_GENERAL comprises of models that are used to train newly constructed features.
-            for model in MODELS_CLASSIFICATION:
-                for i in range(1, count_of_features):
-                    selector = PCA(n_components=i)
-                    selector = selector.fit(train_input, train_output)
+            # Ensemble models with KFOLD
+            l.append(self.train_classifiers(temp_train_input, temp_test_input, train_output, test_output, count))
+            count+=step_size
+        l.append(self.train_classifiers(temp_train_input, temp_test_input, train_output, test_output, count_of_features))
+        output = pd.DataFrame(l)
+        # All the models score with model name is stored in a csv file
+        output.to_csv("output_score.csv", sep=",")
 
-                    temp_train_input = selector.transform(train_input)
-                    temp_test_input = selector.transform(test_input)
 
-                    # Ensemble models with KFOLD
-
-                    model_name = str(model)
-                    model.fit(temp_train_input, train_output)
-                    pred = model.predict(temp_test_input)
-                    temp = {"MODEL_NAME": model_name[:7], " DIMENSION": i,
-                            "RMSE": np.sqrt(mean_squared_error(test_output, pred)),
-                            " R2_SCORE ": r2_score(test_output, pred)}
-                    l.append(temp)
-
-                    if global_mean_score > np.sqrt(
-                            mean_squared_error(test_output, pred)) and global_r2_score < r2_score(
-                            test_output, pred):
-                        global_mean_score = np.sqrt(mean_squared_error(test_output, pred))
-                        global_r2_score = r2_score(test_output, pred)
-
-            output = pd.DataFrame(l)
-            # All the models score with model name is stored in a csv file
-            output.to_csv("output_score.csv", sep=",")
+if __name__ == "__main__":
+    obj = FeatureSelection()
+    dataset = obj.read_data("path")
+    train_input, test_input, train_output, test_output = obj.split_dataset(dataset)
+    obj.pca_model_classification(train_input, test_input, train_output, test_output, 25)
