4c85f0584e719740bc0bc2554050c727b39696d2
thefr33radical <imperial.gauntlet@gmail.com>
2018-09-18 14:39:09 +0530

    18sep18

--- ml_algorithms/word_embedding.py
+++ ml_algorithms/word_embedding.py
@@ -14,9 +14,31 @@ class WordEmbedding(object):
     def __init__(self):
         pass
 
+    def vocab_construction(self,dataset):
+        """
+        Function to compute vocabulary of a corpus, stopwords not removed
+        :param dataset:
+        :return:
+        """
+
+        vocab=set()
+        for doc in dataset:
+            vocab.update((doc.split()))
+        return vocab
+
     def co_occurence(self,datset):
         """
         :param datset: list of strings
         :return: pandas dataframe comprising of co occurence matrix
         """
+        vocab = self.vocab_construction(datset)
+        vocab=list(vocab)
+        co_matrix = pd.DataFrame(columns=vocab,index=vocab)
+        co_matrix.fillna(0,inplace=True)
+        print(co_matrix)
 
+
+if __name__=="__main__":
+    data = ["i am here", "you are there", "lets go to ooty"]
+    obj = WordEmbedding()
+    obj.co_occurence(data)
