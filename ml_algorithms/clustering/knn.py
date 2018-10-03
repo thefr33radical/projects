9a93e1dcdab6a82f7b28a63e1c9e352fb25b5b9d
thefr33radical <imperial.gauntlet@gmail.com>
2018-10-03 16:51:37 +0530

    3oct18

--- ml_algorithms/clustering/knn.py
+++ ml_algorithms/clustering/knn.py
@@ -1,4 +1,6 @@
 
+import numpy as np
+
 
 class Knn(object):
     """
@@ -10,6 +12,22 @@ class Knn(object):
         self.train_output=train_output
         self.test_input=test_input
         self.train_output=test_output
+
+    def compute_eucledian(self,row1,row2,length):
+        """
+        Function to compute eucledian distance
+        :param row1: list
+        :param row2: list
+        :param length: length
+        :return: eucledian distance
+        """
+
+        dist=0
+        for i in range(0,length):
+            dist+= np.square(row1[i]-row2[i])
+
+        dist= np.sqrt(dist)
+        return dist
 
     def fit(self,traiin_input,train_output):
         pass
