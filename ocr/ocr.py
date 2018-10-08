b97c509eabe5c47df08ba97102d1f42e6c853d66
thefr33radical <imperial.gauntlet@gmail.com>
2018-10-08 14:03:38 +0530

    3oct18

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -1,8 +1,20 @@
 import os
 import pandas as pd
+import pyocr
 from PIL import Image, ImageEnhance, ImageFilter
 import pytesseract
 import cv2
+from PIL import Image
+import sys
+
+import pyocr
+import pyocr.builders
+
+# preprocess steps
+# remove noise
+# convert to grayscale
+# dpi scale
+
 
 class OCR(object):
     def __init__(self):
@@ -23,9 +35,73 @@ class OCR(object):
         cv2.imshow('norm_img', cv2.resize(norm_img, (0, 0), fx=0.5, fy=0.5))
         pass
 
+
+    def remove_noise(self,img):
+        pass
+
+    def load_image(self):
+        """
+
+        :return:
+        """
+
+
+        tools = pyocr.get_available_tools()
+        # The tools are returned in the recommended order of usage
+        tool = tools[0]
+
+        langs = tool.get_available_languages()
+        lang = langs[0]
+        # Note that languages are NOT sorted in any way. Please refer
+        # to the system locale settings for the default language
+        # to use.
+
+        txt = tool.image_to_string(
+            Image.open('/home/kuliza227/Downloads/demo300.jpg'),
+            lang="eng",
+            builder=pyocr.builders.TextBuilder()
+        )
+        # txt is a Python string
+        print(txt)
+        '''word_boxes = tool.image_to_string(
+            Image.open('/home/kuliza227/Downloads/IMG_4056_sized.jpg'),
+            lang="eng",
+            builder=pyocr.builders.WordBoxBuilder()
+        )
+        # list of box objects. For each box object:
+        #   box.content is the word in the box
+        #   box.position is its position on the page (in pixels)
+        #
+        # Beware that some OCR tools (Tesseract for instance)
+        # may return empty boxes
+        '''
+        line_and_word_boxes = tool.image_to_string(
+            Image.open('/home/kuliza227/Downloads/IMG_4057_sized.jpg'), lang="eng",
+            builder=pyocr.builders.LineBoxBuilder()
+        )
+        #print(line_and_word_boxes[0])
+        # list of line objects. For each line object:
+        #   line.word_boxes is a list of word boxes (the individual words in the line)
+        #   line.content is the whole text of the line
+        #   line.position is the position of the whole line on the page (in pixels)
+        #
+        # Beware that some OCR tools (Tesseract for instance)
+        # may return empty boxes
+        '''
+        # Digits - Only Tesseract (not 'libtesseract' yet !)
+        digits = tool.image_to_string(
+            Image.open('test-digits.png'),
+            lang=lang,
+            builder=pyocr.tesseract.DigitBuilder()
+        )
+        # digits is a python string
+        '''
+
 if __name__=="__main__":
-    img = cv2.imread('/home/kuliza227/Downloads/IMG_4057.jpg', 0)
-    print((img))
-    #text = pytesseract.image_to_string(Image.open('/home/kuliza227/Downloads/IMG_4057.jpg'),lang="eng")
-    #print(text)
+    obj=OCR()
+    #obj.load_image()
+    #img = cv2.imread('/home/kuliza227/Downloads/IMG_4056_sized.jpg', 0)
+    #print((img))
+    text = pytesseract.image_to_string(Image.open('/home/kuliza227/Downloads/demo2.jpg'),lang="eng")
+    print(text)
     pass
