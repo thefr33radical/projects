f4cde1e612f961657bc7f5539328b89cea13071b
thefr33radical <imperial.gauntlet@gmail.com>
2018-10-16 16:38:12 +0530

    16Oct18

--- ocr/demo_digit_recognition.py
+++ ocr/demo_digit_recognition.py
@@ -1,16 +1,29 @@
 
 from sklearn.datasets import load_digits
-
-dataset =load_digits()
-print(dataset.target.shape,type(dataset))
-
 import numpy as np
 import matplotlib.pyplot as plt
-plt.figure(figsize=(20,4))
-for index, (image, label) in enumerate(zip(dataset.data[0:5], dataset.target[0:5])):
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+dataset =load_digits()
+
+"""
+print(dataset.target.shape,type(dataset))
+
+plt.figure(figsize=(20,10))
+for index, (image, label) in enumerate(zip(dataset.data[0:15], dataset.target[0:15])):
     print(index)
-    plt.subplot(1,5,index+1)
+    plt.subplot(3,5,index+1)
     plt.imshow(np.reshape(image, (8,8)), cmap=plt.cm.gray)
     plt.title('Training: %i\n' % label, fontsize = 20)
 plt.show()
+"""
+
+train_x,test_x,train_y,test_y = train_test_split(dataset.data,dataset.target,test_size= 0.2)
+
+model = LogisticRegression()
+model.fit(train_x,train_y)
+
+print(accuracy_score(model.predict(test_x[0:15]),test_y[0:15]))
 
