11dade4fb5d9a7008afdb78c17ddca23a1d4dc96
thefr33radical <imperial.gauntlet@gmail.com>
2018-10-23 14:54:07 +0530

    23oct18

--- research/flask_demo/demo.py
+++ research/flask_demo/demo.py
@@ -1,30 +1,29 @@
 from flask import jsonify,Flask
 
 
-class demo(object):
+class Demo(object):
     """
         Flask Demo Server
 
     """
-
-    def __init__(self):
-        self.app = Flask(__name__)
-        self.app.add_url_rule("/names","names",self.names,methods=["GET"])
-        self.app.add_url_rule("/","/",self.welcome,methods=["GET"])
-        self.app.run(debug=True,host='0.0.0.0')
-
-
     def names(self):
         """
-        
+
         :return:
         """
         data = {"names": ["John", "Jacob", "Julie", "Jennifer"]}
         return jsonify(data)
 
     def welcome(self):
-        return "<H1> Welcome </H1>"
+        return "<H1> Welcome!!!!!!!!!!!!! </H1>"
+
+
+obj = Demo()
+app = Flask(__name__)
+app.add_url_rule("/names","names",obj.names,methods=["GET"])
+app.add_url_rule("/","/",obj.welcome,methods=["GET"])
+app.run(debug=True,host='0.0.0.0')
 
 
 if __name__ == '__main__':
-    obj = demo()
+    obj = Demo()
