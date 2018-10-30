90ab7c30333531d7384c2ad282aba20f4c697b0f
thefr33radical <imperial.gauntlet@gmail.com>
2018-10-30 13:02:07 +0530

    30oct18

--- research/performance_metrics.py
+++ research/performance_metrics.py
@@ -1,7 +1,25 @@
 
-
+from sklearn.metrics import confusion_matrix
 class PerfMetric(object):
     class Regression(object):
         pass
+
     class Classification(object):
+
+        def confusion_matrix(self,predicted_output,actual_output):
+            """
+
+            :param predicted_output:
+            :param actual_output:
+            :return:
+            """
+
+        def auc(self,predicted_output,actual_output):
+            """
+
+            :param predicted_output:
+            :param actual_output:
+            :return:
+            """
+
         pass
