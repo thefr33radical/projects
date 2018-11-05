fdfaf6e4c96f22183650e07392a93f4f27dd58b2
thefr33radical <imperial.gauntlet@gmail.com>
2018-11-05 15:42:01 +0530

    5nov18

--- ml_algorithms/regression/gradient_descent.py
+++ ml_algorithms/regression/gradient_descent.py
@@ -1,2 +1,20 @@
 
 
+"""
+Gradient Descent Algorithm :
+    1. Statndardize values between  0 and 1
+    2. Assign random values to A and  B in Y = A + X * B
+    3.
+        a.Compute SSEi = 1/2 * Sum<0~n>[  (Y-Yp)^2 ]
+        b.Compute Error Gradients
+            dSSE/dA = -(Y-Yp)
+            dSSE/dB = -(Y-Yp)*X
+        c.Find new A & B
+            New A = A - ( r * dSSE/dA )
+            New B = B -( r * dSSE/dB )
+
+        d.Compute SSEi+1
+
+    4. Stop when (| SSEi-SSEi+1 | ) < minimum_margin
+
+"""
