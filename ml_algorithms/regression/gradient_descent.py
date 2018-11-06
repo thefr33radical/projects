5a37f1a1afde88965055d6b378e02ec116e6d241
thefr33radical <imperial.gauntlet@gmail.com>
2018-11-06 15:49:58 +0530

    6nov18

--- ml_algorithms/regression/gradient_descent.py
+++ ml_algorithms/regression/gradient_descent.py
@@ -31,14 +31,12 @@ class GradDescent(object):
 
         X=np.array([1100.0, 1400,1425,1550,1600,1700,1700,1875,2350,2450])
         Y=np.array([199000,245000,319000,240000,312000,279000,310000,308000,405000,324000])
-
+        data_len = len(X)
         scaler= MinMaxScaler()
         scaler2=StandardScaler()
         X = scaler.fit_transform(X.reshape(-1,1))
         Y = scaler.fit_transform(Y.reshape(-1, 1))
-
         print(" 1. Standardized ", "\n")
-        #print("X ", X, "\nY ", Y)
 
         a = random.choice([0.35,0.45,0.55,0.65,0.75])
         b = random.choice([0.35,0.45,0.55,0.65,0.75])
@@ -46,14 +44,25 @@ class GradDescent(object):
 
         Ypred=[]
 
-        for i in range(len(X)):
+
+        for i in range(0,data_len):
             ypred= a + X[i]*b
             Ypred.append(   float(   (Y[i]- ypred)*(Y[i]- ypred) ) )
 
         #print(Ypred)
         SSE = (sum(Ypred))/2
+        print(" 3. a.SSE:",SSE)
 
-        print(SSE)
+        dSSEa=[]
+        dSSEb=[]
+        for i in range(0,data_len):
+            dSSEa.append( float( -Y[i]-ypred[i] ) )
+
+        for i in range(0,data_len):
+            dSSEb.append( float( -(Y[i]-ypred[i])*X[i] ) )
+
+        print(dSSEa,dSSEb)
+
 
 if __name__ == '__main__':
     obj = GradDescent()
