ec26e509b64c58c79facadaf6345ab77f8ae7bd7
Gowtham Nayak <imperial.gauntlet@gmail.com>
2021-04-23 08:46:52 -0400

    Update cat_Feature_transformer.py

--- others/activity_recognition_multimodal_learning/cat_Feature_transformer.py
+++ others/activity_recognition_multimodal_learning/cat_Feature_transformer.py
@@ -1,10 +1,27 @@
+
+import pandas as pd 
+import prince
+import matplotlib.pylab as plt
 import category_encoders as ce
 
 class FeatureExtract:
-    def __init__(self):
-        self.cat=[]
-        self.ord=[]
-        self.cont=[]
+  def __init__(self):
+    self.cat=[]
+    self.ord=[]
+    self.cont=[]
+
+  def MCAX(data,cat_columns):
+    """
+    cat_columns: list of categorical columns
+    """
+    X=data[[cat_columns]]
+    print(X.head())
+    mca = prince.MCA()
+
+    mca = mca.fit(X) # same as calling ca.fs_r(1)
+    mca = mca.transform(X) # same as calling ca.fs_r_sup(df_new) for *another* test set.
+    print(mca)
+    return mca
 
     def OneHotEncode(self,data,column):
         encoder = ce.OneHotEncoder(cols=[column], return_df=True)
@@ -36,3 +53,7 @@ class FeatureExtract:
             if n.ngroups < 1000 and n.groups >10:
                 l.append(self.FeatureHasher(data, col, 15))
                 continue
+
+  
+
+
